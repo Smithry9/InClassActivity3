3517deff4324f2ebcb330e681684541b1a446dac
Smithry9 <smithry9@oregonstate.edu>
2021-04-30 18:18:51 -0700

    added 3 more test cases, 1 intentionally fails

--- test_calculator.py
+++ test_calculator.py
@@ -2,8 +2,17 @@ import unittest
 import calculator
 
 class testCaseAdd(unittest.TestCase):
-    def test_calc(self):
+    #Tests that should pass
+    def test_calc1(self):
         self.assertEqual(calculator.calc(5,10), (15,-5,50,0.5))
+    def test_calc2(self):
+        self.assertEqual(calculator.calc(4,2), (6,2,8,2.0))
+    def test_calc3(self):
+        self.assertEqual(calculator.calc(1,4), (5,-3,4,0.25))
+
+    #Test that should fail
+    def test_calc4(self):
+        self.assertEqual(calculator.calc(4,8), (1,-1,1,1))
 
 if __name__ == "__main__":
     unittest.main()
